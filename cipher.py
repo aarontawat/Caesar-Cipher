f68a29f54f73eff99fc67e65643f7902fdba9bf5
Aaron <iq180.555@gmail.com>
2019-11-29 12:19:26 +0700

    final

--- cipher.py
+++ cipher.py
@@ -38,7 +38,7 @@ def parse_command_line():
     # Add positional arguments
     parser.add_argument('infile', action='store', help='input file to be encrypted or decrypted')
     # Add optional arguments
-    parser.add_argument('-o', '--outfile_path', action='store', help='output file')
+    parser.add_argument('-o', '--outfile', action='store', help='output file')
     parser.add_argument('-k', '--key', action='store', type=int, default=1, help='encryption/decryption key (must be positive) (default = 1)')
     parser.add_argument('-d', '--decrypt', action='store_true', help='decrypt the input file')
     parser.add_argument('-a','--all', action='store_true', help='decrypt using all keys [1, 25], save outputs indifferent files. (useful in case the key is lost or unknown)')
@@ -48,10 +48,192 @@ def parse_command_line():
     return args
 
 
+def read_file(file_path):
+    """
+    Read file_path and return the content as string.
+    
+    The file must be opened and closed and the function should handle exceptions when they are raised.
+    
+    args:
+        file_path: path to file
+        
+    returns:
+        message: content of file in file_path as a string
+    """
+    
+    #TODO: Your code goes here
+    with open(file_path) as f:
+        read_data = f.read()
+    return read_data
+
+
+def write_file(message, file_path):
+    """
+    Write the message in file_path.
+    
+    The file must be opened and closed and the function should handle exceptions when they are raised.
+    
+    args:
+        message: string to write in file
+        file_path: path to file
+        
+    returns:
+        None
+    """
+    
+    #TODO: Your code goes here
+    f = open(file_path, "w")
+    f.write(message)
+    f.close()
+
+
+def transform(message, key, decrypt):
+    """
+    Encrypt or decrypt a message using Caesar cipher.
+    
+    Encryption and decryption is determined by the Boolean value in decrypt. Key determines the number of 
+    places a character is shifted. When encrypting, use the positive value of key to shift the characters forward; 
+    when decrypting, use the negative key to shift the characters backward. 
+    
+    The function should maintain characters that are not letters without change; for example, spaces, punctuations, 
+    and numbers should not be encrypted or decrypted. Additionally, the case of the letters should be preserved, 
+    small letters are transformed to other small letters and capital letters are transformed to capital letters.
+    
+    Use the function `shift` (provided later) to shift each character in message by the number in key.
+    
+    args:
+        message: string to be encrypted or decrypted
+        key: number of places to shift the characters (always positive)
+        decrypt: Boolean; when False the message is encrypted,  when True the message is decrypted
+        
+    returns:
+        transformed_message: encrypted (or decrypted) message
+        
+    examples:
+        Encryption
+        ==  transform("deal", 1, False) returns:
+            "efbm"
+        
+        ==  transform("deal", 2, False) returns:
+            "fgcn"
+        
+        ==  transform("deal", 30, False) is equivalent to transform(message, 4, False)
+            "hiep"
+        
+        Decryption
+        ==  transform("efbm", 1, True) returns:
+            "deal"
+            
+        ==  transform("fgcn", 2, True) returns:
+            "deal"
+            
+        ==  transform("hiep", 30, True) returns:
+            "deal"    
+        
+    """
+    
+    #TODO: Your code goes here
+    transformed_message = ""
+    if not decrypt:
+        for char in message:
+            if char.isalpha():
+                transformed_message += shift(char,key)
+            else:
+                transformed_message += char
+    else:
+        key = 0 - key
+        for char in message:
+            if char.isalpha():
+                transformed_message += shift(char,key)
+            else:
+                transformed_message += char
+    return transformed_message
+
+
+def shift(char, key):    
+    """
+    Shift char by the value in key while maintaining the case (small/capital).
+    
+    If char contains non-letters (i.e. digits, punctuations, and white spaces), it is ignored.
+    
+    args:
+        char: character to shift
+        key: number of places to shift char
+        
+    returns:
+        shifted character
+        
+    examples:
+        shfit('a', 1) ==> 'b'
+        shift('z', -1) ==> 'y'
+        shift('A', 5) ==> 'F'
+        shift('H', 7) ==> 'O'
+        shift('o', -10) ==> 'e'
+        shift('a', 30) ==> 'e'
+    """
+    
+    # ordered lower case alphabet
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    
+    # will contain shifted lower case alphabet
+    shifted_alphabet = ''
+    for i in range(len(alphabet)):
+        shifted_alphabet = shifted_alphabet + alphabet[(i + key) % 26]
+ 
+    if char.isalpha():
+        char_index = alphabet.index(char.lower())
+        shifted_char = shifted_alphabet[char_index]
+    
+        # keep char's case (upper or lower)
+        if char.isupper():
+            return shifted_char.upper()
+        else:
+            return shifted_char
+
+
 def main():
     # parse command line arguments
     args = parse_command_line()
+    
+    # read content of infile to a string
+    instring = read_file(args.infile)
+    
+    # verbose
+    if args.verbose:
+        print("Input file:")
+        print("------------")
+        print(instring)
+        print()
+    
+    # key is specified
+    if not args.all:
+        # encrypt/decrypt content of infile
+        outstring = transform(instring, args.key, args.decrypt)
+    
+        # verbose
+        if args.verbose:
+            print("Output file:")
+            print("------------")
+            print(outstring)
 
+        # write content of outstring to outfile
+        write_file(outstring, args.outfile)
+    
+    # key is not specified, try all keys from 1 to 25 to decrypt infile
+    else:
+        for k in range(1, 26):
+            # decrypt content of infile
+            outstring = transform(instring, k, True)
 
+            # verbose
+            if args.verbose:
+                print("Key =", k)
+                print("------------")
+                print(outstring)
+                print()
+
+            # write content of outstring to outfile
+            write_file(outstring, "decrypted_by_" + str(k) + ".txt")
+    
 if __name__ == '__main__':
     main()
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+    # parser.add_argument('infile', action='store', help='input file to be encrypted or decrypted')
